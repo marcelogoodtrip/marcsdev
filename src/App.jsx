ac16e0dc1c72b2e7d8f524d2fe6c911909f7f0a5
Marcelo Moura de Lima <marcelogoodtrip@gmail.com>
2023-03-15 10:36:03 -0300

    Include logo-slogan and fixes all styles for this.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './styles/app.sass'
+import Slogan from './assets/marcsdev-logo.png'
 
 function App() {
 
   return (
     <div className="App">
+      <div className="image container">
+        <img src={Slogan} alt="Logo e Slogan" />
+      </div>
     </div>
   )
 }
