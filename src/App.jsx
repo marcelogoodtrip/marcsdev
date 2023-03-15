b3d40ae8da420336aede4b9e908376d9605c46b8
Marcelo Moura de Lima <marcelogoodtrip@gmail.com>
2023-03-15 13:29:19 -0300

    Add home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './styles/app.sass'
-import Slogan from './assets/marcsdev-logo.png'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <div className="App">
-      <div className="image container">
-        <img src={Slogan} alt="Logo e Slogan" />
-      </div>
+      <Home />
     </div>
   )
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Slogan from '../../assets/marcsdev-logo.png'
+import './home.sass'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="slogan-container">
+      <img src={Slogan} className='slogan' alt="Logo e Slogan" />
+    </div>
   )
 }
 
