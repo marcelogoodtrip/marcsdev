4bbc3e3afda392e93628e033dab0b9fee4c33097
Marcelo Moura de Lima <marcelogoodtrip@gmail.com>
2023-03-15 21:30:15 -0300

    Navbar Created

---
+++ src/components/HomeComponents/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.sass'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Quem Sou</li>
+        <li>O Que Faço</li>
+        <li>Portfolio</li>
+        <li>Fale Comigo</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Navbar from '../../components/HomeComponents/Navbar/Navbar'
 import Slogan from '../../assets/marcsdev-logo.png'
 import './home.sass'
 
 const Home = () => {
   return (
-    <div className="slogan-container">
-      <img src={Slogan} className='slogan' alt="Logo e Slogan" />
+    <div className='home-container'>
+      <Navbar />
+      <div className="slogan-container">
+        <img src={Slogan} className='slogan' alt="Logo e Slogan" />
+      </div>
     </div>
   )
 }
